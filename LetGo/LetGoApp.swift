5698f78dee96406781f1d8b19313cd53958b2158
JAEHYUN GO <gojaehyeonstudio@gmail.com>
2025-05-21 12:28:34 +0900

    Initial Commit

---
+++ LetGo/LetGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetGoApp.swift
+//  LetGo
+//
+//  Created by Gojaehyun on 5/21/25.
+//
+
+import SwiftUI
+
+@main
+struct LetGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LetGoTests/LetGoTests.swift
@@ -0,0 +1,17 @@
+//
+//  LetGoTests.swift
+//  LetGoTests
+//
+//  Created by Gojaehyun on 5/21/25.
+//
+
+import Testing
+@testable import LetGo
+
+struct LetGoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
